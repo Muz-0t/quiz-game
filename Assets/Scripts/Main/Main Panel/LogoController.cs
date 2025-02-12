7baf6fbc8a28a11cf9c34a696639b2842c7d0193
Muz-0t <rjsgmltls123@nate.com>
2025-02-12 10:15:32 +0900

    로고 제작

--- Assets/Scripts/Main/Main Panel/LogoController.cs
+++ Assets/Scripts/Main/Main Panel/LogoController.cs
@@ -2,11 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using System;
 
 public class LogoController : MonoBehaviour
 {
-    [SerializeField] private GameObject topLogo;
-    [SerializeField] private GameObject middleLogo;
-    [SerializeField] private GameObject bottomLogo;
+    //Image의 위치정보가 필요하기때문에 RectTansform으로 할당
+    [SerializeField] private RectTransform topLogoRectTransform;
+    [SerializeField] private RectTransform middleLogoRectTransform;
+    [SerializeField] private RectTransform bottomLogoRectTransform;
 
+    private void Start()
+    {
+        Show();
+    }
+    
+    public void Show()
+    {
+        //3장의 로고 이미지의 위치를 변경해서 로고가 입체적으로 보이게 하는 애니메이션 실행
+        topLogoRectTransform.DOAnchorPosY(15f, 2f);
+        bottomLogoRectTransform.DOAnchorPosY(-15f, 2f);
+    }
 }
