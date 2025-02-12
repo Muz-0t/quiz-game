61b9f80f0fe847f6282b43318f4285f826243605
Muz-0t <rjsgmltls123@nate.com>
2025-02-12 10:51:38 +0900

    Main Scene 추가

---
+++ Assets/Scripts/Main/MainPanelController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MainPanelController : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _heartText;       //남은 하트 수
+    [SerializeField] private TMP_Text _stageText;       //현재 스테이지
+    
+    
+    /// <summary>
+    /// Play Button을 눌렀을 때 호출되는 함수
+    /// </summary>
+    public void OnClickPlayButton()
+    {
+        
+    }
+
+    #region Main Menu 버튼 클릭 함수
+    /// <summary>
+    /// Shop 아이콘 터치 시 호출되는 함수
+    /// </summary>
+    public void OnClickShopButton()
+    {
+        
+    }
+    
+    /// <summary>
+    /// Stage 아이콘 터치 시 호출되는 함수
+    /// </summary>
+    public void OnClickStageButton()
+    {
+        
+    }
+    
+    /// <summary>
+    /// LeaderBoard 아이콘 터치 시 호출되는 함수
+    /// </summary>
+    public void OnClickLeaderboardButton()
+    {
+        
+    }
+    
+    /// <summary>
+    /// Settings 아이콘 터치 시 호출되는 함수
+    /// </summary>
+    public void OnClickSettingsButton()
+    {
+        
+    }
+
+    #endregion
+}
