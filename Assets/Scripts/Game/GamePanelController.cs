0d0ffe5cf5a2d174ea0b33dd44521dc6e523fc3f
Muz-0t <rjsgmltls123@nate.com>
2025-02-12 13:42:01 +0900

    objectPool구현

--- Assets/Scripts/Game/GamePanelController.cs
+++ Assets/Scripts/Game/GamePanelController.cs
@@ -1,9 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GamePanelController : MonoBehaviour
 {
+    [SerializeField] private GameObject quizCardPrefab;     //퀴즈 카드의 프리팹
+    [SerializeField] private Transform quizCardParent;      //퀴즈 카드가 표시 될 UI Parent
+
+    private void Start()
+    {
+        ShowQuizCard();
+    }
+
+    private void ShowQuizCard()
+    {
+        var quizCardObject = Instantiate(quizCardPrefab, quizCardParent);
+    }
     public void OnClickGameOverButton()
     {
         GameManager.Instance.QuitGame();
