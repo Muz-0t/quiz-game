9398a6c2014a1b9d0c29961a5917086d746c6446
Muz-0t <rjsgmltls123@nate.com>
2025-02-12 09:26:58 +0900

    Main 씬 추가 및 테스트

---
+++ Assets/Scripts/Main/Main Panel/LogoController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class LogoController : MonoBehaviour
+{
+    [SerializeField] private GameObject topLogo;
+    [SerializeField] private GameObject middleLogo;
+    [SerializeField] private GameObject bottomLogo;
+
+}
