58f31c7312115b53c8ee365e4964601f513711c8
Muz-0t <rjsgmltls123@nate.com>
2025-02-14 17:00:18 +0900

    MuzTimer.cs 기능 구현, 일시정지, 리셋키 구현

--- Assets/Scripts/Timer/MuzTimer.cs
+++ Assets/Scripts/Timer/MuzTimer.cs
@@ -1,8 +1,75 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MuzTimer : MonoBehaviour
 {
+    [System.Serializable]
+    public class FillSettings
+    {
+        public Color color;
+    }
+
+    public FillSettings fillSettings;
+
+    [Serializable]
+    public class BackGroundSettings
+    {
+        public Color color;
+    }
+    public BackGroundSettings backGroundSettings;
     
+    [SerializeField] private Image fillImage;
+    [SerializeField] private float totalTime;
+    [SerializeField] private TMP_Text timeText;
+    
+    public float CurrentTime{get; private set;}
+    private bool _isPaused;
+
+    private void Start()
+    {
+        StartTimer();
+    }
+
+    private void Update()
+    {
+        if (!_isPaused)
+        {
+            CurrentTime += Time.deltaTime;
+            fillImage.fillAmount = Math.Clamp((CurrentTime / totalTime),0,1);
+            var remainTime = Math.Max(0,totalTime-CurrentTime);
+            timeText.text = remainTime.ToString("F1");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PauseTimer();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ResetTimer();
+        }
+    }
+
+    public void StartTimer()
+    {
+        fillImage.fillAmount = 0;
+        _isPaused = false;
+    }
+
+    public void PauseTimer()
+    {
+        _isPaused = !_isPaused;
+    }
+
+    public void ResetTimer()
+    {
+        CurrentTime = 0;
+        fillImage.fillAmount = 0;
+    }
+
 }
