942a2ff7cdedd780373448bbf82b3943b8343e95
Muz-0t <rjsgmltls123@nate.com>
2025-02-25 14:50:14 +0900

    stage cell 오브젝트 풀 방식으로 구현

--- Assets/Scripts/Main/StagePopupPanelController.cs
+++ Assets/Scripts/Main/StagePopupPanelController.cs
@@ -2,12 +2,17 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(PopupPanelController))]
 public class StagePopupPanelController : MonoBehaviour
 {
     [SerializeField] private GameObject stageCellPrefab;
     [SerializeField] private Transform contentTransform;
+    
+    private ScrollRect _scrollRect;
+    private RectTransform _rectTransform;
+    
     private void Start()
     {
         GetComponent<PopupPanelController>().SetTitleText("STAGE");
@@ -18,7 +23,10 @@ public class StagePopupPanelController : MonoBehaviour
         //Stage Cell 만들기
         for (int i = 0; i < maxStageCount; i++)
         {
-            GameObject stageCellObject = Instantiate(stageCellPrefab, contentTransform);
+            GameObject stageCellObject = ObjectPool.Instance.GetObject();
+            stageCellObject.transform.SetParent(contentTransform, false);
+            stageCellObject.SetActive(true);
+            
             StageCellButton stageCellButton = stageCellObject.GetComponent<StageCellButton>();
             //stageCellButton.SetStageCell(i, StageCellButton.StageCellType.Normal);
 
@@ -50,4 +58,5 @@ public class StagePopupPanelController : MonoBehaviour
         
         Debug.Log(yPos);
     }
+    
 }
